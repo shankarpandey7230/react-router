4598deec3102dc0cb33c2de1e15f7781af49254c
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-27 00:41:32 +1000

    NavLink

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import StyledNavbar from "../components/StyledNavbar";
 
 const SharedLayout = () => {
   return (
     <>
-      <Navbar />
+      <StyledNavbar />
       {/* <section className="section"> */}
       {/* <h2>Home Page</h2> */}
       {/* <Link to="/about" className="btn">
