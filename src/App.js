b8ed3cf6084ea318d22790836e552b8254a2f074
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-27 02:16:25 +1000

    protected Route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SingleProduct from "./pages/SingleProduct";
 import { useState } from "react";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -22,7 +23,14 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<SingleProduct />} />
           <Route path="login" element={<Login setUser={setUser} />} />
-          <Route path="dashboard" element={<Dashboard user={user} />} />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user} />
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
