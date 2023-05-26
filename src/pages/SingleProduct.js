0213bd2ed0359378d11f74d6c8d5d203332653a9
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-27 01:30:30 +1000

    useParams setup

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { Link, useParams } from "react-router-dom";
+import products from "../data";
 const SingleProduct = () => {
-  return <h2>SingleProduct</h2>;
+  const { productId } = useParams();
+
+  return (
+    <section className="section product">
+      <h2>{productId}</h2>
+      <Link to="/products">Back to Products</Link>
+    </section>
+  );
 };
 
 export default SingleProduct;
